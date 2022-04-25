b156064a6587a949582b90578d98e609362ff1ca
hsnbsrn <hasan1510c@gmail.com>
2022-04-26 01:45:00 +0300

    Update AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NdComponent } from './nd/nd.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [		
     AppComponent,
-      NdComponent
+      NdComponent,
+      LoginComponent
    ],
   imports: [
     BrowserModule,
